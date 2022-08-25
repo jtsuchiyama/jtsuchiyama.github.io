25106e012d16770118d3afd952c714180da2a496
Jake Tsuchiyama <jtsuchiyama@scu.edu>
2022-08-25 11:18:11 -1000

    main: Header styling

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,14 +3,17 @@ import './Header.css';
 function Header() {
     return (
         <header>
-            <div className="title">Jake Tsuchiyama</div>
-            <div className="nav">
-                <a href="/">Home</a>
-                <a href="/projects">Projects</a>
-                <a href="/music">Music</a>
+            <div>
+                <div className="title">Jake Tsuchiyama</div>
+                <div className="nav">
+                    <a href="/">Home</a>
+                    <a href="/projects">Projects</a>
+                    <a href="/music">Music</a>
+                </div>
             </div>
+            
         </header>
     )
 }
 
-export default Header;
+export default Header;  
