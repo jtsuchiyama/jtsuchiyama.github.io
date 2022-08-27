da30fd95734c2922c0e4c2db6ab07d4c1d2143d1
Jake Tsuchiyama <jtsuchiyama@scu.edu>
2022-08-27 12:39:17 -1000

    main: Header mobile nav

--- src/components/Header/Navbar.js
+++ src/components/Header/Navbar.js
@@ -1,12 +1,35 @@
 import './Navbar.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBars } from '@fortawesome/free-solid-svg-icons';
+import { useState } from 'react';
 
 function Navbar() {
+    const [isNavExpanded, setIsNavExpanded ] = useState(false);
+
     return (
-        <button>
-        <FontAwesomeIcon icon={faBars} />
-        </button>
+        <div class="nav">
+            <button onClick={() => {
+                setIsNavExpanded(!isNavExpanded);
+            }}>
+                <FontAwesomeIcon icon={faBars} size="lg" />
+            </button>
+            <div className={isNavExpanded ? "nav-menu expanded" : "nav-menu"}>
+                <ul>
+                    <li>
+                        <a href="/">Home</a>
+                    </li>
+                    <li>
+                        <a href="/projects">Projects</a>
+                    </li>
+                    <li>
+                        <a href="/music">Music</a>
+                    </li>
+                    <li>
+                        <a href="/aboutme">More About Me</a>
+                    </li>
+                </ul>
+            </div>
+        </div> 
     )
 }
 
