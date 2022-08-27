617eea768f69ac87f84c28c9f07a73a0c30eb76b
Jake Tsuchiyama <jtsuchiyama@scu.edu>
2022-08-27 10:21:19 -1000

    main: Header updates

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,15 +3,15 @@ import './Header.css';
 function Header() {
     return (
         <nav>
-            <div>
-                <div className="title">Jake Tsuchiyama</div>
-                <div className="nav">
-                    <a href="/">Home</a>
-                    <a href="/projects">Projects</a>
-                    <a href="/music">Music</a>
-                </div>
+            <div className="links links-left">
+                <a href="/">Home</a>
+                <a href="/projects">Projects</a>
             </div>
-            
+            <div className="title">Jake Tsuchiyama</div>
+            <div className="links links-right">
+                <a href="/music">Music</a>
+                <a href="/aboutme">More About Me</a>
+            </div>
         </nav>
     )
 }
