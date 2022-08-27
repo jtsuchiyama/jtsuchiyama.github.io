3b70587417c4dcf2c521e0545083abd942634818
Jake Tsuchiyama <jtsuchiyama@scu.edu>
2022-08-27 10:20:44 -1000

    main: Footer CSS

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -5,12 +5,12 @@ import { faLinkedin, faGithub, faYoutube } from '@fortawesome/free-brands-svg-ic
 function Footer() {
     return (
         <footer>
+            <div className="copyright">Jake Tsuchiyama</div>
             <div className="icons">
                 <a href="https://www.linkedin.com/in/jake-tsuchiyama" target="_blank" rel="noreferrer"><FontAwesomeIcon icon={faLinkedin} /></a>
                 <a href="https://github.com/jtsuchiyama" target="_blank" rel="noreferrer"><FontAwesomeIcon icon={faGithub} /></a>
                 <a href="https://www.youtube.com/c/jtkulele" target="_blank" rel="noreferrer"><FontAwesomeIcon icon={faYoutube} /></a>
             </div>
-            <div className="copyright">© 2022 Jake Tsuchiyama</div>
         </footer>
     )
 }
