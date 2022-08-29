312052b856cf1fe22c688a362207e913e942df8d
Jake Tsuchiyama <jtsuchiyama@scu.edu>
2022-08-29 13:01:38 -1000

    main: Class rename

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -8,7 +8,7 @@ function Header() {
                 <a href="/">Home</a>
                 <a href="/projects">Projects</a>
             </div>
-            <div className="title">Jake Tsuchiyama</div>
+            <div className="header-title">Jake Tsuchiyama</div>
             <div className="links links-right">
                 <a href="/music">Music</a>
                 <a href="/aboutme">More About Me</a>
