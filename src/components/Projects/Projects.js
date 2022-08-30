41d6e092c9a32f9bd284916e621e4af4a91d1d8c
Jake Tsuchiyama <jtsuchiyama@scu.edu>
2022-08-29 17:31:44 -1000

    main: Updating projects page

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,16 +1,15 @@
 import './Projects.css';
 import Card from './Card.js';
-import covidtracker from './images/covidtracker.png';
+import drifting from './images/drifting.png'
+import ukulelecv from './images/ukulele-cv.png';
 
 function Projects() {
     return (
         <div>
             <div className="projects-headline">Projects</div>
             <div className="projects">
-                <Card title='Covid Tracker' lang='Python' tech='Beautiful Soup, Pandas, Matplotlib, Twilio' body='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua' link='https://github.com/' image={covidtracker} alt='Covid Tracker'></Card>
-                <Card title='Covid Tracker' body='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua' link='https://github.com/' image={covidtracker} alt='Covid Tracker'></Card>
-                <Card title='Covid Tracker' body='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua' link='https://github.com/' image={covidtracker} alt='Covid Tracker'></Card>
-                <Card title='Covid Tracker' body='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua' link='https://github.com/' image={covidtracker} alt='Covid Tracker'></Card>
+                <Card title='Drifting in the Water' role='Project Manager / Lead Game Developer' lang='C#, Unity' tech='N/A' body='Led a team of 13 students with an Agile approach to create a 2D side-scrolling bullet hell game that spreads awareness of ocean pollution solutions through gameplay mechanics and messaging' link='https://github.com/SCUGuildGamers/GuildGamers' image={drifting} alt='Drifting in the Water'></Card>
+                <Card title='Ukulele Chord Recognition' role='Lead Engineer' lang='Python' tech='OpenCV' body='Led a team of 13 students with an Agile approach to create a 2D side-scrolling bullet hell game that spreads awareness of ocean pollution solutions through gameplay mechanics and messaging' link='https://github.com/jtsuchiyama/ukulele-cv' image={ukulelecv} alt='Ukulele Chord Recognition'></Card>
             </div>
         </div>
     )
