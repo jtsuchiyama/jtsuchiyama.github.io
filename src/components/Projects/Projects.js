e5129fa94dcd8f91b131cbcc98da27dc3d4b8aa0
Jake Tsuchiyama <jtsuchiyama@scu.edu>
2022-08-29 17:06:09 -1000

    main: Centering projects

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -8,6 +8,7 @@ function Projects() {
             <Card title='Covid Tracker' lang='Python' tech='Beautiful Soup, Pandas, Matplotlib, Twilio' body='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua' link='https://github.com/' image={covidtracker} alt='Covid Tracker'></Card>
             <Card title='Covid Tracker' body='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua' link='https://github.com/' image={covidtracker} alt='Covid Tracker'></Card>
             <Card title='Covid Tracker' body='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua' link='https://github.com/' image={covidtracker} alt='Covid Tracker'></Card>
+            <Card title='Covid Tracker' body='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua' link='https://github.com/' image={covidtracker} alt='Covid Tracker'></Card>
         </div>
     )
 }
