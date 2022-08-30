762a809cad6bbbca85a6ae67d83608b011660b45
Jake Tsuchiyama <jtsuchiyama@scu.edu>
2022-08-29 17:11:32 -1000

    main: Projects headline

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -4,11 +4,14 @@ import covidtracker from './images/covidtracker.png';
 
 function Projects() {
     return (
-        <div className="projects">
-            <Card title='Covid Tracker' lang='Python' tech='Beautiful Soup, Pandas, Matplotlib, Twilio' body='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua' link='https://github.com/' image={covidtracker} alt='Covid Tracker'></Card>
-            <Card title='Covid Tracker' body='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua' link='https://github.com/' image={covidtracker} alt='Covid Tracker'></Card>
-            <Card title='Covid Tracker' body='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua' link='https://github.com/' image={covidtracker} alt='Covid Tracker'></Card>
-            <Card title='Covid Tracker' body='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua' link='https://github.com/' image={covidtracker} alt='Covid Tracker'></Card>
+        <div>
+            <div className="projects-headline">Projects</div>
+            <div className="projects">
+                <Card title='Covid Tracker' lang='Python' tech='Beautiful Soup, Pandas, Matplotlib, Twilio' body='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua' link='https://github.com/' image={covidtracker} alt='Covid Tracker'></Card>
+                <Card title='Covid Tracker' body='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua' link='https://github.com/' image={covidtracker} alt='Covid Tracker'></Card>
+                <Card title='Covid Tracker' body='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua' link='https://github.com/' image={covidtracker} alt='Covid Tracker'></Card>
+                <Card title='Covid Tracker' body='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua' link='https://github.com/' image={covidtracker} alt='Covid Tracker'></Card>
+            </div>
         </div>
     )
 }
