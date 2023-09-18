8e5bc48470b8d61d9368a36894953ba42cc67edc
Jake Tsuchiyama <61369356+jtsuchiyama@users.noreply.github.com>
2023-09-18 00:05:21 -0700

    Routing?

--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
 import React from 'react';
-import { HashRouter } from "react-router-dom";
 import ReactDOM from 'react-dom/client';
 import App from './components/App/App.js';
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
     <React.StrictMode>
-        <HashRouter>
+        <BrowserRouter>
             <App />
-        </HashRouter>
-    </React.StrictMode>,
-    document.getElementById('root')
+        </BrowserRouter>
+    </React.StrictMode>
 );
